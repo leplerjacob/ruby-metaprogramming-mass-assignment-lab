ca6d0303135b39592072abf5c57320d7ca964784
Jacob Lepler <lepler.jacob@gmail.com>
2021-01-22 18:56:36 -0500

    Done.

--- lib/person.rb
+++ lib/person.rb
@@ -1,3 +1,21 @@
 class Person
-  #your code here
+  
+  def initialize(attributes)
+    attributes.each do |key, value|
+      self.class.attr_accessor(key)
+      self.send(("#{key}="), value)
+    end
+  end
+
+  def save
+    sql = <<-SQL
+      INSERT INTO person (name, grade)
+    SQL
+  end
+
+  def self.create_person(hash)
+    person = Person.new(hash)
+    person.save
+    person
+  end
 end
